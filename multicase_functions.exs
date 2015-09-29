62a3a3c7477dbe6a2a622ee6b070b0447a9b9065
Pedro Antonio <petterlx727@gmail.com>
2015-09-29 01:46:21 -0500

    Multicase functions exercise

---
+++ multicase_functions.exs
@@ -0,0 +1,16 @@
+multi_fizzbuzz = fn
+  (0 , 0, _) -> "FizzBuzz"
+  (0, _, _) -> "Fizz"
+  (_, 0, _) -> "Buzz"
+  (_, _, c) -> c
+end
+
+fizzbuzz = fn n -> multi_fizzbuzz.(rem(n, 3), rem(n, 5), n) end
+
+IO.puts fizzbuzz.(10)
+IO.puts fizzbuzz.(11)
+IO.puts fizzbuzz.(12)
+IO.puts fizzbuzz.(13)
+IO.puts fizzbuzz.(14)
+IO.puts fizzbuzz.(15)
+IO.puts fizzbuzz.(16)
