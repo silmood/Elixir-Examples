32dc01c1249dc21c1253f100637fa4c56ad34aa3
Pedro Antonio <petterlx727@gmail.com>
2015-12-31 16:07:40 -0600

    Calculator server sum method was implemented

---
+++ Modules/database_server.exs
@@ -0,0 +1,32 @@
+defmodule DatabaseServer do
+
+  def start do
+    spawn(&loop/0)
+  end
+
+  def loop do
+    receive do
+      {:run_query, caller, query_def} ->
+        send(caller, {:query_result, run_query(query_def)})
+    end
+    loop
+  end
+
+  defp run_query(query_def) do
+    :timer.sleep(2000)
+    " #{query_def} result"
+  end
+
+  def run_async(server_pid, query_def) do
+    send(server_pid, {:run_query, self, query_def})
+  end
+
+  def get_result do
+    receive do
+      {:query_result, result} -> result
+    after 5000 ->
+      {:error, :timeout}
+    end
+  end
+
+end
--- calculator_server.exs
+++ calculator_server.exs
@@ -1,11 +1,24 @@
 defmodule CalculatorServer do
   def start do
-    spawn(&loop/0)
+    spawn(fn ->
+      value = 0
+      loop(value)
+    end)
   end
 
-  def loop do
-    receive do
-      #To define
+  def loop(value) do
+    new_value = receive do
+      :get_value -> IO.inspect value
+      {:sum, x}-> x + value
     end
+    loop(new_value)
+  end
+
+  def value(server_pid) do
+    send(server_pid, :get_value)
+  end
+
+  def sum(server_pid, x) do
+    send(server_pid, {:sum, x})
   end
 end
